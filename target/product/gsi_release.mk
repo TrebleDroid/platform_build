efdd0dc917852ff779a3102c1a7b79149680702e
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2021-12-08 07:04:53 +0000

    Remove init.vndk-nodef.rc
    This partially reverts "Deprecate VNDK-lite support from Legacy GSI".
    
    Paired with https://github.com/LineageOS/android_system_core/commit/0b265e35ddf42638fa807f5349a10c40e3d46446, it kicks permissive devices into bootloader.
    Given that GSI+permissive won't ever be a Lineage official scenario, revert this only on our side.
    Thanks @Kethen for the insight!
    
    Change-Id: I7c14fe5229e953f620bb225fa5c981752d0ac5f9

--- target/product/gsi_release.mk
+++ target/product/gsi_release.mk
@@ -48,8 +48,6 @@ PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
 PRODUCT_PACKAGES += \
     gsi_skip_mount.cfg \
     init.gsi.rc \
-    init.vndk-nodef.rc \
-
 
 # Overlay the GSI specific setting for framework and SystemUI
 ifneq ($(PRODUCT_IS_AUTOMOTIVE),true)
