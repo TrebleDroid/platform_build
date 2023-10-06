95aaad8650b769a9283074ef2defd7a763af7dcf
Pierre-Hugues Husson <phh@phh.me>
2023-10-06 19:11:22 -0400

    Include vndk v28 sepolicy support

--- core/config.mk
+++ core/config.mk
@@ -874,6 +874,7 @@ BOARD_SEPOLICY_VERS := $(PLATFORM_SEPOLICY_VERSION)
 
 # A list of SEPolicy versions, besides PLATFORM_SEPOLICY_VERSION, that the framework supports.
 PLATFORM_SEPOLICY_COMPAT_VERSIONS := \
+    28.0 \
     29.0 \
     30.0 \
     31.0 \
